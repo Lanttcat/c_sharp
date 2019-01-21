df50866bcc45be3b2eaf47cac8c0588a199b24db
nikomaniac <niko.sky.lan@gmail.com>
2019-01-21 22:59:37 +0800

    test for char encoding

--- c_sharp/test/BasicTypeFact.cs
+++ c_sharp/test/BasicTypeFact.cs
@@ -12,5 +12,23 @@ namespace c_sharp.test
             var todo2 = '\u0000';
             Assert.Equal(todo1, todo1);
         }
+
+        [Fact]
+        public void should_get_new_line_for_string()
+        {
+            var todo = @"a
+b";
+            Assert.Equal("a\nb", todo);
+        }
+
+        [Fact]
+        public void should_get_var_value_for_string()
+        {
+            var todo1 = "Ingram";
+            var todo2 = $"my name is {todo1}";
+            var expectValue = $"my name is Ingram";
+            
+            Assert.Equal(expectValue, todo2);
+        }
     }
 }
