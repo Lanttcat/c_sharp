aeb739ff4e51c4edc818558d5688a8c105b3a8c3
nikomaniac <niko.sky.lan@gmail.com>
2019-01-24 22:57:56 +0800

    test for command

---
+++ c_sharp/src/designPatterns/command/Command1.cs
@@ -0,0 +1,10 @@
+namespace c_sharp.designPatterns.command
+{
+    public class Command1 : ICommand
+    {
+        public void Execute()
+        {
+//            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ c_sharp/src/designPatterns/command/ICommand.cs
@@ -0,0 +1,8 @@
+namespace c_sharp.designPatterns.command
+{
+    public interface ICommand
+    {
+
+        void Execute();
+    }
+}
